b4f76015393edaea63b9604323db8ab984eab824
aashimagogia <42136860+aashimagogia@users.noreply.github.com>
2019-02-28 16:46:42 +0530

    Code Formatting

--- stihlusa.py
+++ stihlusa.py
@@ -1,38 +1,24 @@
 
 # coding: utf-8
 
-# In[1]:
-
-
+# Importing Libraries:
+import pandas as pd
 from requests import get
 from bs4 import BeautifulSoup
 
-
-# In[3]:
-
-
+# Extracting Product's Data:
 product = []
 link = []
 for i in range(1,14):
-    Soup_html = get("https://www.stihlusa.com/products/stihl-top-rated-products/?page="+str(i))
+    Soup_html = get("https://www.stihlusa.com/products/stihl-top-rated-products/?page={}".format(i))
     soup = BeautifulSoup(Soup_html.text,"html.parser")
     for k in soup.find_all(class_=["clickable-product"]):
         product.append(k.get("data-name"))
     for p in soup.find_all(class_=["clickable-product-element"]):
         link.append(p.get("href"))
-
-
-# In[4]:
-
-
-#creating dataframe
-import pandas as pd
-df = pd.DataFrame({"product_name":product,"product_url":link[0::2]})
-#df.to_excel("SthilUSA")
-
-
-# In[5]:
-
-
+      
+# creating dataframe
+df = pd.DataFrame({"product_name": product, "product_url": link[0::2]})
+COde
+# creating .csv file:
 df.to_csv('product.csv',index=False)
-
