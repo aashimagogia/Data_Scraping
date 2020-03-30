06b3e24577f330e2454a9711a10ed76f2a6a4fc9
aashima <aashimagogia@gmail.com>
2020-03-31 00:22:01 +0530

    modifying data scraping code

--- stihlusa.py
+++ stihlusa.py
@@ -1,18 +1,14 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
+# Importing libraries
 from requests import get
 from bs4 import BeautifulSoup
+import pandas as pd
 
-
-# In[3]:
-
-
+# initiating lists
 product = []
 link = []
+
+# scraping data
 for i in range(1,14):
     Soup_html = get("https://www.stihlusa.com/products/stihl-top-rated-products/?page="+str(i))
     soup = BeautifulSoup(Soup_html.text,"html.parser")
@@ -22,17 +18,8 @@ for i in range(1,14):
         link.append(p.get("href"))
 
 
-# In[4]:
-
-
 #creating dataframe
-import pandas as pd
 df = pd.DataFrame({"product_name":product,"product_url":link[0::2]})
-#df.to_excel("SthilUSA")
-
-
-# In[5]:
-
-
+df.to_excel("SthilUSA")
 df.to_csv('product.csv',index=False)
 
